06d3d3fd6ab747cca7485a0e1bced72d67fdb452
Natallia Dubrouskaya <Natallia_Dubrouskaya@epam.com>
2017-01-20 12:26:48 +0300

    using sass

---
+++ task_3/gulpfile.babel.js
@@ -0,0 +1,15 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import path from 'path';
+
+gulp.task('sass', function() {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass({
+        paths: [ path.join(__dirname, 'sass', 'includes') ]
+    }))
+    .pipe(gulp.dest('./build/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', ['sass'])
+});
